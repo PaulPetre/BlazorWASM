ce601f997e35a9434b5534dd537f99596e946522
Paul Petre <paul.petre98@yahoo.com>
2021-04-17 00:58:40 +0300

    Change project from BlazorWASM to Model...

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
-using System;
-using System.Net.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
+using Blazored.Modal;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Blazor.BwipJs;
 
-namespace BlazorWASM
+namespace ModalPopupWindowWASM
 {
     public class Program
     {
@@ -17,8 +19,9 @@ namespace BlazorWASM
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+            builder.Services.AddBlazorBwipJs();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddBlazoredModal();
             await builder.Build().RunAsync();
         }
     }
