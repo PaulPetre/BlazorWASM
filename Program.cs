34d6b52b0aa0309040d28c2edd0f1ccdd29798b5
Paul Petre <paul.petre98@yahoo.com>
2021-04-17 02:22:59 +0300

    c

--- Program.cs
+++ Program.cs
@@ -3,14 +3,14 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using BlazorWASM;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using Blazor.BwipJs;
 
-namespace ModalPopupWindowWASM
+namespace BlazorWASM
 {
     public class Program
     {
