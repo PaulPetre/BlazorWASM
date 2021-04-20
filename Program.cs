3732f264646097b97e36f211abc55df1b40a3957
Paul <paul.petre98@yahoo.com>
2021-04-20 09:39:59 +0300

    Resolved some bugs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Blazored.Modal;
+using MudBlazor.Services;
 
 namespace BlazorWASM
 {
@@ -20,6 +21,7 @@ namespace BlazorWASM
 
             builder.Services.AddBlazoredModal();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
